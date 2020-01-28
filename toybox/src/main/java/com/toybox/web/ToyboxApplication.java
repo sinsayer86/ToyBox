8c6d2b2ccc6d6eec0fa34b405f286b90b63ca358
서유리 <seoyuli@218.38.137.28>
2020-01-28 17:35:23 +0900

    init

---
+++ toybox/src/main/java/com/toybox/web/ToyboxApplication.java
@@ -0,0 +1,13 @@
+package com.toybox.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyboxApplication.class, args);
+	}
+
+}
---
+++ toybox/src/test/java/com/toybox/web/ToyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toybox.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
